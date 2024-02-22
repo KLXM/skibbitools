2aae4a6da74ee2f4c186ed4740d3df81fb6b7801
Thomas Skerbis <thomas.skerbis@mac.com>
2024-02-22 10:17:20 +0100

    Update klxmdefaults.php

--- lib/klxmdefaults.php
+++ lib/klxmdefaults.php
@@ -1,40 +1,44 @@
 <?php
-
 class SkibbiTools
 {
     public static function cke5LightboxHelper(): void
     {
-        if (rex::isFrontend()) { {
-                // Code hier einfügen
+        if (rex::isFrontend()) {{
+            // Code hier einfügen
 
-                rex_extension::register('OUTPUT_FILTER', function (rex_extension_point $ep) {
-                    $html = $ep->getSubject();
+            rex_extension::register('OUTPUT_FILTER', function (rex_extension_point $ep) {
+                $html = $ep->getSubject();
 
-                    // Verwende reguläre Ausdrücke, um verlinkte Bilder im HTML zu finden
-                    preg_match_all('/<figure\b[^>]*\bclass\s*=\s*["\'][^"\']*?\bimage\b[^"\']*["\'][^>]*>.*?<a[^>]+href=[\'"]([^\'"]+?\.(jpg|jpeg|png|mp4|gif))[\'"][^>]*><img[^>]+src=[\'"]([^\'"]+?)[\'"][^>]*>.*?<\/figure>/i', $html, $matches, PREG_SET_ORDER);
+                // Verwende reguläre Ausdrücke, um verlinkte Bilder im HTML zu finden
+                preg_match_all('/<figure\b[^>]*\bclass\s*=\s*["\'][^"\']*?\bimage\b[^"\']*["\'][^>]*>.*?<a[^>]+href=[\'"]([^\'"]+?\.(jpg|jpeg|png|mp4|gif))[\'"][^>]*><img[^>]+src=[\'"]([^\'"]+?)[\'"][^>]*>.*?<\/figure>/i', $html, $matches, PREG_SET_ORDER);
 
-                    // Durchlaufe alle Treffer
-                    foreach ($matches as $match) {
-                        // Hole die abgeglichenen Werte
-                        $link = $match[0];
-                        $href = $match[1];
-                        $ext = $match[2];
-                        $src = $match[3];
+                // Durchlaufe alle Treffer
+                foreach ($matches as $match) {
+                    // Hole die abgeglichenen Werte
+                    $link = $match[0];
+                    $href = $match[1];
+                    $ext = $match[2];
+                    $src = $match[3];
 
-                        // Überprüfe, ob der href-Wert auf .jpg, .jpeg, .png oder .gif endet
-                        if (in_array($ext, ['jpg', 'jpeg', 'png', 'mp4', 'gif'])) {
-                            // Wenn es ein Bild ist, ersetze das <figure>-Tag mit der aktualisierten Version
-                            $updated_link = str_replace('<figure ', '<figure uk-lightbox ', $link);
-                            $html = str_replace($link, $updated_link, $html);
-                        }
+                    // Überprüfe, ob der href-Wert auf .jpg, .jpeg, .png oder .gif endet
+                    if (in_array($ext, ['jpg', 'jpeg', 'png', 'mp4', 'gif'])) {
+                        // Wenn es ein Bild ist, ersetze das <figure>-Tag mit der aktualisierten Version
+                        $updated_link = str_replace('<figure ', '<figure uk-lightbox ', $link);
+                        $html = str_replace($link, $updated_link, $html);
                     }
-                    // Setze das geänderte HTML als neues Subjekt
-                    $ep->setSubject($html);
-                }, rex_extension::LATE);
-            }
+                }
+                // Setze das geänderte HTML als neues Subjekt
+                $ep->setSubject($html);
+            }, rex_extension::LATE);
+        }
         }
     }
 
+/** 
+ * @param string $file
+ * @param string $type
+ *  @return string
+ */
     public static function mediaCopyright(string $file = '', string $type = 'text'): string
     {
         $output = '';
@@ -50,6 +54,25 @@ class SkibbiTools
             }
         }
         return $output;
+    }
+
+    /**
+     * @param string $file
+     * @param string $alt
+     * @return string
+     */
+    public static function mediaAlt(string $file = '', string $alt = ''): string
+    {
+        $output = '';
+        if ($alt != '') {
+            return $alt;
+        }
+        if ($media = rex_media::get($file)) {
+            $alt = $media->getValue('med_description');
+            if ($alt !== '') {
+                return $alt;
+            } else {return $output;}
+        }
     }
 
     public static function truncateText(string $string, $count = 300): string
@@ -74,14 +97,21 @@ class SkibbiTools
         return $teaser;
     }
 
-
+    /**
+     * @param string $date
+     * @return string
+     */
     public static function formatGermanDate(string $date): string
     {
         $formatter = new \IntlDateFormatter('de_DE', \IntlDateFormatter::LONG, \IntlDateFormatter::NONE);
         return $formatter->format(strtotime($date));
     }
 
-    public static function checkUrl(?string $url): ?string
+    /**
+     * @param string $date
+     * @return string
+     */
+     public static function checkUrl(?string $url): ?string
     {
         if ($url) {
             if (filter_var($url, FILTER_VALIDATE_URL) === false) {
